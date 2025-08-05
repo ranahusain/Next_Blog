ce80516f7e806ae62bcb1b4102a125844f069170
ranahusain <capthussain2point0@gmail.com>
2025-08-05 12:10:23 +0500

    leftbar done

--- components/BlogPage/LeftSideBar.tsx
+++ components/BlogPage/LeftSideBar.tsx
@@ -1,5 +1,49 @@
+import styles from "./BlogPage.module.css";
+import Link from "next/link";
+import { LuLibrary } from "react-icons/lu";
+import { BsPerson } from "react-icons/bs";
+import { IoHomeOutline } from "react-icons/io5";
+import { IoHome } from "react-icons/io5";
 const LeftSideBar = () => {
-  return <div>LeftSideBar</div>;
+  return (
+    <>
+      <div>
+        <div className={styles.left_nav}>
+          <Link href="#" className="flex gap-4 ">
+            <IoHome className={styles.leftbar_icon} />
+            Home
+          </Link>
+          <Link href="#" className="flex gap-4">
+            <LuLibrary className={styles.leftbar_icon} />
+            Library
+          </Link>
+          <Link href="#" className="flex gap-4">
+            <BsPerson className={styles.leftbar_icon} />
+            Profile
+          </Link>
+          <hr className="text-gray-300" />
+
+          <Link href="/" className="flex gap-4 mt-10">
+            <b>Follwing &nbsp; &gt; </b>
+          </Link>
+
+          <Link href="#" className="flex gap-4 mt-10 text-gray-600">
+            <BsPerson className="text-2xl " />
+            <p className="text-sm">
+              Discover more writers <br />
+              and publications to <br />
+              follow.
+              <br />
+              <br />
+              <span className="text-underline">
+                <u>See Suggestions</u>
+              </span>
+            </p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LeftSideBar;
